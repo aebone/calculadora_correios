41a2ace4fd79930df2aeb7a4af68c0d685c67357
Aline Ebone <ini.ebone@gmail.com>
2017-03-26 15:15:03 -0300

    Home and Result pages functional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,13 +9,17 @@ class HomeController < ApplicationController
     # get params from index page
     @from = params[:from]
     @to = params[:to]
+    @peso = params[:peso].gsub(',', '.')
+    @comprimento = params[:comprimento]
+    @largura = params[:largura]
+    @altura = params[:altura]
 
     frete = Correios::Frete::Calculador.new :cep_origem => @from,
                                             :cep_destino => @to,
-                                            :peso => 0.3,
-                                            :comprimento => 30,
-                                            :largura => 15,
-                                            :altura => 2
+                                            :peso => @peso.to_f,
+                                            :comprimento => @comprimento,
+                                            :largura => @largura,
+                                            :altura => @altura
     @sedex = frete.calcular :sedex
     @pac = frete.calcular :pac
 
