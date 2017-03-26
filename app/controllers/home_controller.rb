e4fb9fb671a18e7075b0d4fd12c8de15935447c1
Aline Ebone <ini.ebone@gmail.com>
2017-03-25 23:39:09 -0300

    Home page ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,24 @@
 require 'correios-frete'
 
 class HomeController < ApplicationController
-  def index
   
-    frete = Correios::Frete::Calculador.new :cep_origem => "04094-050",
-                                            :cep_destino => "90619-900",
+  def index  
+  end
+
+  def result
+    # get params from index page
+    @from = params[:from]
+    @to = params[:to]
+
+    frete = Correios::Frete::Calculador.new :cep_origem => @from,
+                                            :cep_destino => @to,
                                             :peso => 0.3,
                                             :comprimento => 30,
                                             :largura => 15,
                                             :altura => 2
     @sedex = frete.calcular :sedex
-    @sedex.nome         
-    @sedex.descricao     
-    @sedex.valor         
-    @sedex.prazo_entrega 
-
     @pac = frete.calcular :pac
-    @pac.nome          
-    @pac.descricao     
-    @pac.valor         
-    @pac.prazo_entrega      
 
-    # get params from index page
-    @from = params[:from]
-    @to = params[:to]
   end
 
-  def result
-  end
 end
